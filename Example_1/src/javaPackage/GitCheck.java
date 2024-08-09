7df8e0c4b3a491aaa85e5640a20e09c466669b66
Rupesh <rupesh.dew@gmail.com>
2024-08-09 09:09:53 +0530

    added new files

--- Example_1/src/javaPackage/GitCheck.java
+++ Example_1/src/javaPackage/GitCheck.java
@@ -4,7 +4,25 @@ public class GitCheck {
 
 	public static void main(String[] args) {
 
-
+		String s = "aaaabbcccdd";
+		int count =0;
 		
+		char[] ch = s.toCharArray();
+
+				for(int i =0; i< ch.length; i++)
+				{
+					for(int j= i+1; j<ch.length; j++)
+					{
+						if (ch[i]==ch[j])
+						{
+							count++;	
+
+						}
+						
+					}
+					
+					System.out.println(ch[i]+""+count);
+				}
+				
 	}
 }
